7dae73ed76b79f2b8179eadfbf49a2345aa6fd9d
Soban Nadeem <soban312004@gmail.com>
2024-11-14 21:15:24 +0500

    make it better

--- database/db.js
+++ database/db.js
@@ -2,17 +2,13 @@ import mongoose from "mongoose";
 
 const Connection = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://soban312004:f5dX6xmCLskUCYGg@cluster0.hbeih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
-
+    await mongoose.connect(process.env.URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Database connected successfully");
   } catch (error) {
-    console.log("Error while connecting to the database ", error);
+    console.log("Error while connecting to the database", error);
   }
 };
 
