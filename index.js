42735633c4dc81520afed67535414ea4ab3c4bfe
Soban Nadeem <soban312004@gmail.com>
2024-11-14 21:58:02 +0500

    changes

--- index.js
+++ index.js
@@ -11,7 +11,13 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Frontend's URL
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
