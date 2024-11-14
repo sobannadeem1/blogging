0a0c5583d54d191a3005de8f6e68346f56cb3912
Soban Nadeem <soban312004@gmail.com>
2024-11-14 17:10:11 +0500

    Update MongoDB connection with environment variables

--- database/db.js
+++ database/db.js
@@ -1,12 +1,15 @@
 import mongoose from "mongoose";
 
 const Connection = async () => {
-  const URL = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.hbeih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
   try {
-    await mongoose.connect(URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(
+      `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.hbeih.mongodb.net/blogging?retryWrites=true&w=majority&appName=Cluster0`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+
     console.log("Database connected successfully");
   } catch (error) {
     console.log("Error while connecting to the database ", error);
