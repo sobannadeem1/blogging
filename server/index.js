e46675495df09ddb68658c26d10c38ab87491f31
Soban Nadeem <soban312004@gmail.com>
2024-11-15 12:20:04 +0500

    Describe your changes here

--- server/index.js
+++ server/index.js
@@ -1,27 +1,33 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import bodyParser from 'body-parser';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
 
 //components
-import Connection from './database/db.js';
-import Router from './routes/route.js';
-
+import Connection from "./database/db.js";
+import Router from "./routes/route.js";
 
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
+
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/', Router);
+app.use("/", Router);
 
+const PORT = process.env.PORT || 8000;
 
-const PORT = 8000;
-const username = process.env.DB_USERNAME;
-const password = process.env.DB_PASSWORD;
+Connection();
+if (process.env.NODE_ENV === "production") {
+  // Serve React static files
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve("client", "build", "index.html"));
+  });
+}
 
-Connection(username, password);
-
-app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server is running successfully on PORT ${PORT}`)
+);
